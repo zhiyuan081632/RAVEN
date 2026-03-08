30be4f54096c761693d81d138099e00a5a94ca59
root <root@zhihui.localdomain>
2026-03-08 10:51:01 +0800

    update feat failed file

--- src/test.sh
+++ src/test.sh
@@ -2,4 +2,4 @@ python -W ignore test.py \
     --visual_encoder AVHuBERT_TalkNet_concatenate \
     --test_condition one_interfering_speaker \
     --test_snr -10 \
-    --ckpt_path "/mnt/e/project/prjANS/src/AVSE/RAVEN/src/checkpoints/ckpt_20260307112935/AVHuBERT_TalkNet_concatenate_5layer/last.ckpt"
+    --ckpt_path "/mnt/e/project/prjANS/src/AVSE/train/ckpt_20260308000953/AVHuBERT_VSRiW_concatenate_5layer/last.ckpt"
